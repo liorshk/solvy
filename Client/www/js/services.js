3e7662761cae79a9393707bb1c1e1ea604e4d8eb
Lior Shkiller <lshkiller@gmail.com>
2014-04-16 15:10:28 +0300

    asd

--- Client/www/js/services.js
+++ Client/www/js/services.js
@@ -15,29 +15,30 @@ angular.module('starter.services', [])
             },
 
             register: function () {
-                $http
-                   .post('http://54.72.160.154/AddUser', user)
-                   .success(function (data, status, headers, config) {
-                       if (data)
-                       {
-                           $state.go('login');
-                       }
-                       //$window.sessionStorage.token = data.token;
-                       
-                   })
-                   .error(function (data, status, headers, config) {
-                       // Erase the token if the user fails to log in
-                       //delete $window.sessionStorage.token;
-
-                       // Handle signup errors here
-                       $scope.errormessage = 'Error';
-                   });
+                $http({
+                    url: 'http://localhost/AddUser',
+                    method: "POST",
+                    data: user,
+                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+                })
+                .success(function (response) {
+                    
+                    // success
+                    if (response) {
+                        $state.go('login');
+                    }
+                })
+                .error(function (response) { // optional
+                        // failed
+                        $scope.errormessage = 'Error';
+                    }
+                );
             },
 
             login: function(user)
             {
                     $http
-                    .post('http://54.72.160.154/login', user)
+                    .post('http://localhost/login', user)
                     .success(function (data, status, headers, config) {
                         //$window.sessionStorage.token = data.token;
                         $state.go('tabs.home');
