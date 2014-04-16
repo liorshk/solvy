90b8af21d3a2c0c4169b8cc6cb6a590e956b60df
Lior Shkiller <lshkiller@gmail.com>
2014-04-16 15:12:18 +0300

    ad

---
+++ Server/nodeserver/routes/question.js
@@ -0,0 +1,73 @@
+﻿
+exports.QuestionModule = function(db)
+{
+    var question = db.Node.registerModel( 'Question', { 
+		fields: {
+			defaults: {
+			created_on: function()  {
+				return new Date().getTime();
+			}
+			}
+		}
+		})
+
+    // /*
+    // * POST to adduser.
+    // */
+    //this.AddUser = function(req, res) {		
+	//	var data = req.params;
+	//	if(req.method == "POST")
+	//	{
+	//		data = req.body;
+	//	}
+	//	var user = new User(data);
+
+    //    try {
+    //        user.save(function(err, result) {
+    //            res.json(true);
+    //            });
+
+    //    } catch (err) 
+    //    {
+    //        console.log('Failed to add user: ' + err);
+    //        res.json(false);
+    //    }
+	//}
+        
+    ///*
+    //* POST to LogIn.
+    //*/
+    //this.LogIn = function(req, res) {		
+    //    try 
+    //    { 
+    //        db.Node.findOne( { email: req.body.email }, function(err, dave) {                
+    //        if (err)
+    //        {
+    //            console.error('Error with user login: ' + err);
+    //            res.json(false);                
+    //            return ;
+    //        }
+    //        else
+    //        {
+    //            if (dave != null && dave.data.password == req.body.password)
+    //            {
+    //                console.log('User & Password Matched');
+    //                res.json(true);
+    //                return ;
+    //            }
+    //            else
+    //            {
+    //                console.log('User & Password Unmatched');
+    //                res.json(false);
+    //            }
+    //        }
+    //    })
+    //    }
+    //    catch (err2)
+    //    {
+    //        console.log('Error with user login: ' + err2);
+    //        res.json(false);
+    //    }
+    //};
+
+}
